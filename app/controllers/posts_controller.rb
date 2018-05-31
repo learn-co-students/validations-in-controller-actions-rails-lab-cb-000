e90f576ddfcc7ef2bd7551ce1d93f5076360fd74
Chris Cardle <cdcardle@gmail.com>
2018-05-31 18:11:46 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,16 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    @post.title = params[:title]
+    @post.category = params[:category]
+    @post.content = params[:content]
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private

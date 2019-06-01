383d6a6bee13fc0927f84689a7d8b1a097141a6a
kenkuts <kennethchoi19@gmail.com>
2019-06-01 18:38:04 -0400

    completed task

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true # The name cannot be blank
+  validates :email, uniqueness: true # The e-mail is unique
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true # The title cannot be blank
+  validates :category, inclusion: { in: %w( Fiction Non-Fiction ) } # cat is either fict or non-fict
+  validates :content, length: { minimum: 100 }# The content is at least 100 characters long.
 end
